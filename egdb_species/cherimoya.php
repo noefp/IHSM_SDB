0de786d7e4c076169e8b4cd2f09252127d873528
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-06-15 17:18:22 +0200

    species info added

---
+++ egdb_species/cherimoya.php
@@ -0,0 +1,67 @@
+<a href="/jbrowse/?data=data/anche102" class="btn btn-info" role="button">Genome Browser</a>
+<a href="/easy_gdb/tools/blast/blast_input.php" class="btn btn-info" role="button">BLAST</a>
+<a href="/easy_gdb/downloads.php#Annonacherimola" class="btn btn-info" role="button">Downloads</a>
+<a href="/easy_gdb/tools/fasta_download.php" class="btn btn-info" role="button">Sequence Extraction</a>
+<a href="/easy_gdb/tools/annot_input_list.php" class="btn btn-info" role="button">Annotation Extraction</a>
+<br>
+<br>
+<p>
+  <i>Annona cherimola</i> is a diploid species (2n=2x=14), which originated in Mesoamerica. 
+	It is a deciduous tree and it shows hermaphroditic flowers with protogynous dichogamy, recurrent in basal angiosperm.
+	It is denominated as Cherimoya or "chirimoya" in South America and "Anona" in Central America as other closely related species. 
+	In fact, this word was used by Linnaeus to name the genus Annona, but referred to it as the Latin Word "annona" which means "the harvest of a year". 
+	This perennial fruit crop has been cultivated and extremely appreciated since pre-Columbian times of the Maya, Olmec or/and Izapa civilizations.
+</p>
+<p>
+	Spain is the principal cherimoya commercial producer country with about 3,000 ha followed by Peru and Chile. 
+	Although cherimoya is mainly cultivated in family orchards in Peru, Bolivia and Ecuador, 
+	this crop is also cultivated at small scale in California (USA), Argentina, Madeira (Portugal), Ecuador, Bolivia, South Africa,
+	Australia, Italy, Morocco, Algeria, Egypt and Israel.
+</p>
+<p>
+	The <a href="https://www.ihsm.uma-csic.es/" target="_blank">Institute of Subtropical and Mediterranean Horticulture (IHSM)</a> comprehends the largest germplasm collection of varieties of <i>A. cherimola</i>, 
+	with 334 accessions.
+</p>
+<p>
+  Recently, the interest in this family has risen exponentially due to the presence of important compounds with medicinal properties, 
+	some of them exclusive in the Annonaceae, as polyketide secondary metabolites acetogenins, presents especially in seed and leaves, 
+	with antitumor, antimalarial and pesticidal properties.
+</p>
+
+<h3>Files</h3>
+
+<div class="row">
+  <div class="col">
+    <div class="card" style="width:100%">
+      <div class="card-body">
+        <a href="/downloads/pdfs/descriptores para chirimoyo.pdf"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Descriptores para chirimoyo</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" style="width:100%">
+      <div class="card-body">
+        <a href="/downloads/pdfs/descriptors for cherimoya.pdf"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Descriptors for cherimoya</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" style="width:100%">
+      <div class="card-body">
+        <a href="/downloads/pdfs/guiachirimoyo_mapa_gob.pdf"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Guia de gestion de plagas</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- <div style="height:80px"></div> -->
+<br>
+
+<h3>Genome assembly stats</h3>
+
+<p>
+	The <i>A. cherimola</i> genome cv. Fino de Jete was published in <a href="#">Talavera et al. 2022</a>.
+</p>
+
+<?php include_once("cherimoya_genome_table.php");?>
---
+++ egdb_species/cherimoya_genome_table.php
@@ -0,0 +1,43 @@
+
+      <table class="table table-bordered">
+         <thead>
+           <tr>
+             <th></th>
+             <th>Scaffolds</th>
+             <th>Chromosomes</th>
+           </tr>
+         </thead>
+         <tbody>
+           <tr>
+             <td>Total assembly size (Gb)</td>
+             <td>1.12</td>
+             <td>1.13</td>
+           </tr>
+           <tr>
+             <td>Number of sequences</td>
+             <td>15,076</td>
+             <td>2,052</td>
+           </tr>
+           <tr>
+             <td>Longest sequence (Mb)</td>
+             <td>3.439</td>
+             <td>212.25</td>
+           </tr>
+           <tr>
+             <td>Average length (Mb)</td>
+             <td>0.074</td>
+             <td>0.55</td>
+           </tr>
+           <tr>
+             <td>N50</td>
+             <td>1.314</td>
+             <td>3</td>
+           </tr>
+           <tr>
+             <td>N50 Length (Mb)</td>
+             <td>0.171</td>
+             <td>170.85</td>
+           </tr>
+         </tbody>
+       </table>
+      
