9cf81427db84b831fddaf1a589e04898be198769
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-10-26 10:04:39 +0200

    added citation

---
+++ egdb_custom_text/db_citation.php
@@ -0,0 +1,16 @@
+<div class="row">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+    <h4>Please, when using the cherimoya genome data, cite us using the reference:</h4>
+
+    <div class="card bg-light" style="height:100px">
+      <div class= "card-body">    
+        Talavera A, Fernández-Pozo N, Matas AJ, Hormaza JI, Bombarely A (2023).
+        Genomics in neglected and underutilized fruit crops: A chromosome-scale genome sequence of cherimoya (Annona cherimola)<br>
+        <a href="https://doi.org/10.1002/ppp3.10366" target="blank">Plants, People, Planet. DOI: 10.1002/ppp3.10366
+</a><br>
+      </div>
+    </div>
+
+  </div>
+</div>
