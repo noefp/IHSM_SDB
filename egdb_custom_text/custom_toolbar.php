54f11adb878313737eab89528ede4a1db588a999
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-11-05 15:57:56 +0100

    links to expression and passport enabled

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -1,13 +1,17 @@
 <li class="nav-item dropdown">
-  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">More</a>
+  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Expression Atlas</a>
   <div class="dropdown-menu">
-    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=genome.php">genome</a>
-    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=other page.php">other page</a>
-    <!-- <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=other page.php">other page</a> -->
-    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=table_to_page.php&table_name=tables/Table Example.txt&link_field=ACC Name" >Table Example</a>
-    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=table_menu.php">Table Menu</a>
-
-
+    <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_input.php">Experiment Expression Viewer</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/expression/comparator_input.php">Expression comparator</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_menu.php">Datasets</a>
+  </div>
+</li>
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Private</a>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="/easy_gdb/tools/passport/view_subdirectories.php?dir_name=IHSM_Subtropicals_DB">IHSM Germplasm</a>
+    <!-- <a class="dropdown-item" href="/easy_gdb/tools/passport/passport_search_input.php">Passport and Phenotype Search</a> -->
+    <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_input_private.php">Private Expression Atlas</a>
   </div>
 </li>
 
