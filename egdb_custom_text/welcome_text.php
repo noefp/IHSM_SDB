b859f58164265f70a2284739c84f8fba0570b5f7
Enrique López Gómez <enrique.elg.93@gmail.com>
2025-02-26 17:28:49 +0100

    Update welcome  and db_citation

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -1,18 +1,19 @@
-      <!-- <h1>Welcome to <?php// echo "$dbTitle";?> </h1> -->
-      <h1>Welcome to the IHSM Subtropical Fruits Database</h1>
-      <!-- <h4 class="p_font18">
-        This is a template to write your own welcome page.
-        Please, follow the instructions to create your own custom_text folder and include the path in the configuration file.
-      </h4> -->
-      <p>
-        The Institute of Subtropical and Mediterranean Horticulture (<a href="https://www.ihsm.uma-csic.es/ihsm" target="_blank">IHSM</a>) "La Mayora"
-				is a research center with more than 60 years of history and with a Research station of 50 Ha. Located in the province of Malaga, in southern Spain,
-				it host a collection of varieties of subtropical species adapted to grow in the local environmental conditions (Mediterranean climate), outside of the tropics.
-				The IHSM germplasm collection has the largest number of cherimoya acccessions of the world, and counts with more than 100 accessions of avocado 
-				and more than 80 of mango, among other species.
-				<br>
-				This site provides bioinformatics tools and genomics data of subtropical species maintained and studied at the IHSM.
-      </p>
+<div class="width900">
+  <br>
 
-      <!-- <img class='rounded' src='<?php// echo "$images_path/header_img3.jpg";?>' width="900px" alt='img example'> -->
-      
+    <h1 class="text-center">Welcome to IHSM<span style="color:#9c5;">Subtropicals</span> Fruits Database</h1>
+    <p class="text-justify">
+      The Institute of Subtropical and Mediterranean Horticulture (<a href="https://www.ihsm.uma-csic.es/ihsm" target="_blank">IHSM</a>) "La Mayora" is a research center with more than 60 years of history and with a Research station of 50 Ha. Located in the province of Malaga, in southern Spain, it host a collection of varieties of subtropical species adapted to grow in the local environmental conditions (Mediterranean climate), outside of the tropics.
+      <br>
+      The IHSM germplasm collection has the largest number of cherimoya acccessions of the world, and counts with more than 100 accessions of avocado and more than 80 of mango, among other species.
+      <br>
+      This site provides bioinformatics tools and genomics data of subtropical species maintained and studied at the IHSM.
+    </p>
+    <br>
+
+    <?php 
+      include_once realpath("$custom_text_path/db_citation.php");
+    ?>
+
+  <br>
+</div>
