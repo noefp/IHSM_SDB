f6176bbabaf99017a415b2d0a062085a9ee8b971
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2025-05-08 17:26:37 +0200

    new landing page

--- egdb_custom_text/about.php
+++ egdb_custom_text/about.php
@@ -1,6 +1,22 @@
-<p class="p_font18">
-  Welcome to our About Us page.
-</p>
+
+  <br>
+
+  <h1 style="font-size:32px;" class="text-center">Welcome to IHSM<span style="color:#9c5;">Subtropicals</span> Fruits Database</h1>
+
+  <p style="font-size:18px;margin-top:20px;" class="text-justify">
+    The Institute of Subtropical and Mediterranean Horticulture (<a href="https://www.ihsm.uma-csic.es/ihsm" target="_blank">IHSM</a>) "La Mayora" is a research center with more than 60 years of history and with a Research station of 50 Ha. Located in the province of Malaga, in southern Spain, it host a collection of varieties of subtropical species adapted to grow in the local environmental conditions (Mediterranean climate), outside of the tropics. The IHSM germplasm collection has the largest number of cherimoya acccessions of the world, and counts with more than 100 accessions of avocado and more than 80 of mango, among other species.
+    <br>
+    This site provides bioinformatics tools and genomics data of subtropical species maintained and studied at the IHSM.
+  </p>
+  <br>
+
+  <?php 
+    include_once realpath("$custom_text_path/db_citation.php");
+  ?>
+
+  <br>
+
+
 <p>
   Our group's activity revolves around the sustainable utilization of genetic resources in subtropical fruit trees, considering both current conditions and future climate change projections. This initiative stands as a unique endeavor within the CSIC. We collect detailed knowledge of plant materials, advanced cultivation techniques, and the utilization of genomic tools to pursue the following specific objectives:
 </p>
--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -1,18 +1,86 @@
-<div class="width900">
+<div class="header-content">
   <br>
-
-  <h1 style="font-size:32px;" class="text-center">Welcome to IHSM<span style="color:#9c5;">Subtropicals</span> Fruits Database</h1>
-
-  <p style="font-size:18px;margin-top:20px;" class="text-justify">
-    The Institute of Subtropical and Mediterranean Horticulture (<a href="https://www.ihsm.uma-csic.es/ihsm" target="_blank">IHSM</a>) "La Mayora" is a research center with more than 60 years of history and with a Research station of 50 Ha. Located in the province of Malaga, in southern Spain, it host a collection of varieties of subtropical species adapted to grow in the local environmental conditions (Mediterranean climate), outside of the tropics. The IHSM germplasm collection has the largest number of cherimoya acccessions of the world, and counts with more than 100 accessions of avocado and more than 80 of mango, among other species.
-    <br>
-    This site provides bioinformatics tools and genomics data of subtropical species maintained and studied at the IHSM.
-  </p>
+  <h1 class="text-center">Welcome to IHSM<span style="color:#9c5;">Subtropicals</span></h1>
+  <h4>A Genomic Portal and Germplasm Navigator for Subtropical Species</h4>
   <br>
-
-  <?php 
-    include_once realpath("$custom_text_path/db_citation.php");
-  ?>
-
-  <br>
+  <a href="/easy_gdb/about.php" class="btn-find-out-more no-link-style" style="color: white;">Find out more</a>
 </div>
+
+  
+<style>
+
+  .page_container {
+    padding-left: 0px !important;
+    padding-right: 0px !important;
+    height: 95vh;
+    background: url("<?php echo $images_path.'/landing1.jpg' ?>") no-repeat center center/cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 5%;
+    position: relative;
+    background-size: cover;
+    text-align: center;
+    box-sizing: border-box;
+  }
+
+  .header-content {
+      max-width: 1200px;
+      margin-bottom: 50px;
+  }
+
+  h1 {
+      font-size: 5em;
+      font-weight: bold;
+      margin: 0;
+      line-height: 1.2;
+      color: #fff;
+      text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.7);
+  }
+
+  h4 {
+      font-size: 1.5em;
+      margin: 30px 0;
+      line-height: 1;
+      color: #fff;
+      text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.7);
+  }
+
+  .btn-find-out-more {
+      display: inline-block;
+      padding: 15px 30px;  
+      background-color: #459919;   
+      color: white;
+      text-align: center;
+      text-decoration: none;
+      border-radius: 5px;
+      font-size: 18px;
+  }
+
+  .btn-find-out-more:hover, 
+  .btn-find-out-more:focus {
+      text-decoration: none;  
+      color: white;  
+      background-color: #69a925;  
+  }
+
+  .no-link-style {
+      text-decoration: none;  
+      color: inherit;  
+  }
+
+  .no-link-style:active, .no-link-style:focus, .no-link-style:visited {
+      color: inherit;
+      text-decoration: none;   
+      outline: none;  
+  }
+
+  #gdb_footer {
+    margin-top: 0px !important;
+/*    position: fixed !important;*/
+    bottom: 0 !important;
+  }
+
+
+</style>
