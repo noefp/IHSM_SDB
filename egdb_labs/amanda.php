ad8073cab636f944475a1ef5d39c6eb85003ad4c
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2025-05-09 13:23:10 +0200

    added person to about

---
+++ egdb_labs/amanda.php
@@ -0,0 +1,16 @@
+
+
+<img class="float-right" src="<?php echo $images_path.'/people/amanda.jpg' ?>" style="margin-left:20px">
+<h3>Amanda Bullones</h3>
+<p>
+  <b>Position:</b> PhD Student<br>
+  <b>email:</b> amanda.bullones@csic.es<br>
+  <b>Phone:</b> (+34) 952134177<br>
+
+  <a href="https://twitter.com/IHSM_Bioinfo" target="_blank"><img src="<?php echo $images_path.'/logos/twitter_icon.png' ?>" style="height:16px; margin-bottom: 5px;"> Twitter</a>
+  <br>
+</p>
+<p>
+  My research focuses on the study of the molecular mechanisms of plant responses to abiotic stresses, such as drought, salinity and high temperatures, in Mediterranean species.
+  I have experience in RNA-seq data analysis, including quality control, preprocessing, read alignment, quantification, differential gene expression, correlations and functional enrichment.
+</p>
