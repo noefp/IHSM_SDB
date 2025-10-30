bf15d31ae9833f6852deb34f415460d0d08be075
Noe Fernandez Pozo <noefernandezpozo@macbookpro.home>
2025-10-30 16:13:25 +0100

    pawpaw experiment description added

---
+++ egdb_custom_text/expr_datasets/pawpaw.php
@@ -0,0 +1,45 @@
+<p>
+	RNA-Seq of pawpaw (<i>Asimina triloba</i>) early developing seeds collected from three cross-compatible (CC) 
+  and three auto-incompatible (AI) pollinated flowers at 4, 8 and 15 days after pollination (DAP), resulting in a total of 18 biological replicates. 
+  The pistils of each flower were dissected to obtain the developing seeds, and extract RNA from these fresh early seeds.
+</p>
+
+<h4>Experimental Conditions:</h4>
+
+<ul>
+  <li>Developing seeds of cross-compatible pollinated flowers at 4 DAP (CC4)</li>
+  <li>Developing seeds of auto-incompatible pollinated flowers at 4 DAP (AI4)</li>
+  <li>Developing seeds of cross-compatible pollinated flowers at 8 DAP (CC8)</li>
+  <li>Developing seeds of auto-incompatible pollinated flowers at 8 DAP (AI8)</li>
+  <li>Developing seeds of cross-compatible pollinated flowers at 15 DAP (CC15)</li>
+  <li>Developing seeds of auto-incompatible pollinated flowers at 15 DAP (AI15)</li>
+</ul>
+
+
+<span style="margin-left:120px">CC</span><span style="margin-left:240px">AI</span><br>
+<img class='rounded' src='<?php echo "$images_path/expr/CC_4DAP.jpg";?>' width="250px" alt='img example'>
+<img class='rounded' src='<?php echo "$images_path/expr/AI_4DAP.jpg";?>' width="250px" alt='img example'> 4 DAP
+<br>
+<img class='rounded' src='<?php echo "$images_path/expr/CC_8DAP.jpg";?>' width="250px" alt='img example'>
+<img class='rounded' src='<?php echo "$images_path/expr/AI_8DAP.jpg";?>' width="250px" alt='img example'> 8 DAP
+<br>
+<img class='rounded' src='<?php echo "$images_path/expr/CC_15DAP.jpg";?>' width="250px" alt='img example'>
+<img class='rounded' src='<?php echo "$images_path/expr/AI_15DAP.jpg";?>' width="250px" alt='img example'> 15 DAP
+
+<br>
+
+<p>
+  Microscope images of developing seeds of cross-compatible (on the left) and auto-incompatible crosses (on the right).
+</p>
+
+<p>
+<b> CC </b> = compatible crosses
+<br>
+<b> AI </b> = Autoincompatible crosses
+<br>
+<b> DAP </b> = Days After Pollination
+</p>
+
+<p>
+This dataset was published by <a href="" target="_blank">Ferrer-Blanco et al. 2025</a>, and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/PRJEB97068" target="_blank">PRJEB97068</a>.
+</p>
