27eb75268c76c83ee02e85ea179a89fd4329ecb8
Enrique López Gómez <enrique.elg.93@gmail.com>
2025-11-03 11:25:35 +0100

    Annona INO experiment added

---
+++ egdb_custom_text/expr_datasets/06 Annona Outer Integument.php
@@ -0,0 +1,39 @@
+<p>
+	RNA-Seq of pistils with and without early seed development in Atemoya (<i>Annona squamosa</i> × <i>A. cherimola</i>) cv. Gefner, and the <i>A. squamosa</i> "<i>Thai seedless</i>" mutant (<i>Annona ino</i>).
+</p>
+
+<h4>Experimental Conditions:</h4>
+
+<ul>
+	<li><b>AG</b> = Atemoya cv. Gefner<br>
+	<li><b>T</b> = "<i>Thai seedless</i>" mutant<br>
+	<li><b>PS</b> = Pistils containing early developing seeds (Pollinated pistils with seed).</li>
+	<li><b>P</b> = Pistils without early developing seeds (Pollinated pistils, seed removed).</li>
+</ul>
+
+<p>
+	Total RNA was extracted from pollinated pistils containing early seeds (PS) and pistils without early seeds (P) of <i>Annona cherimola</i> cv. Gefner (AG) and the "<i>Thai seedless</i>" mutant (T), combining the CTAB protocol by <a href="http://dx.doi.org/10.4236/ajps.2016.73037">Doyle and Doyle, 1987</a> and the Qiagen RNeasy Plant Mini Kit (Qiagen).
+	<br>
+	PS and P samples were collected from three independent fruits at 4 and 8 days after pollination (DAP). 
+	Microscopic observations were performed at 8 DAP to illustrate early embryo and nucellus development.
+</p>
+
+<img class='rounded' src='<?php echo "$images_path/expr/INO_AG8_PS.jpg";?>' width="250px" alt='AG8 PS'>
+<img class='rounded' src='<?php echo "$images_path/expr/INO_T8_PS.jpg";?>' width="250px" alt='T8 PS'>
+
+<p>
+	Microscope images of the immature seed sections (8 DAP) for both AG and T genotypes.
+</p>
+
+<p>
+<b>e</b> = endosperm<br>
+<b>ii</b> = inner integument<br>
+<b>n</b> = nucellus<br>
+<b>oi</b> = outer integument<br>
+Scale bars: 100 μm<br>
+</p>
+
+<p>
+This dataset was published by <a href="https://onlinelibrary.wiley.com/doi/full/10.1111/ppl.70538" target="_blank">Garcia-Lezama et al., 2025</a> in <i>Physiologia Plantarum</i>.
+Raw sequencing data are available in the BioProject <a href="https://www.ebi.ac.uk/ena/browser/view/PRJEB97455" target="_blank">PRJEB97455</a>.
+</p>
