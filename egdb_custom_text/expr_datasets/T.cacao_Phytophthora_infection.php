fbb16401aac75db83c6459dbaaa05f627bf22952
Noe Fernandez Pozo <noefernandezpozo@macbookpro.home>
2026-02-03 13:42:38 +0100

    cacao atlas

---
+++ egdb_custom_text/expr_datasets/T.cacao_Phytophthora_infection.php
@@ -0,0 +1,14 @@
+<p>
+  RNA-seq of <i>Theobroma cacao</i> plant tissues from cv. Matina, 7 different varieties:
+</p>
+<h4>Experimental Conditions (3 replicate):</h4>
+  <ul>
+    <li>Pod non infected</li>
+    <li>Pod infected with Phytophthora palmivora</li>
+  </ul>
+<p>
+  RNA-Seq libraries were generated from cacao pod RNA, sequenced on an Illumina platform, and mapped to the reference genome; gene expression levels were normalized using TPM values for comparative analysis across genotypes and treatments.
+  
+  This dataset was published by <a href="https://doi.org/10.1038/s41598-024-54355-8" target="_blank">Baruah et al. 2024 </a> 
+  and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA971242" target="_blank">PRJNA487154</a>.
+</p>
