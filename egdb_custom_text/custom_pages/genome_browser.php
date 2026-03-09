73c0cd218bfe3a158fc904ac0aeb338dd962fe49
Enrique López Gómez <enrique.elg.93@gmail.com>
2026-03-09 17:51:07 +0100

    Genome browser page added

---
+++ egdb_custom_text/custom_pages/genome_browser.php
@@ -0,0 +1,235 @@
+<!-- Help -->
+<div>
+  <a class="float-right" href="https://genome.ucsc.edu/goldenPath/help/hgTracksHelp.html" target="_blank">
+    <i class='fa fa-info' style='font-size:20px;color:#229dff'></i> Help
+  </a>
+</div>
+<br>
+
+<div class="width900">
+  <div class="row">
+    <div class="col-12">
+
+      <!-- Genome Browser -->
+      <h1 class="text-center">Genome Browser <i class="fas fa-dna" style="color:#555"></i><i class="fas fa-search" style="color:#555"></i></h1>
+      <br>
+      <div id="tool-container" class="form margin-20" style="margin:auto; max-width:900px">
+
+
+
+      <!-- Instructions -->
+      <div id="color_default\" class="alert alert-primary" role="alert" style="display:block;margin:0px">
+        <div class="card-body" style="padding:0px;text-align:center">
+          To explore the genomic data of different species, please select one of the options below. 
+          <br>
+          Check out the <a href="/easy_gdb/species.php" target="_blank">species page</a> for additional information and options.
+        </div>
+      </div>
+
+      <br>
+
+      <!-- List -->
+      <div class="row">
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Annona cherimola</i></h5>
+          <div class="list-group">
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/anche102&tracks=DNA%2Canche102_genes" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              A. cherimola 'Fino de Jete'
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Acherimola_booth&tracks=DNA%2CAcherimola_booth_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              A. cherimola 'Booth'
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Annona</i> spp.</h5>
+          <div class="list-group">
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Asquamosa&tracks=DNA%2CAsquamosa_gene_models" target="_blank" data-toggle="modal" data-target="#jbrowse_unavailable"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"> <!--Add jbrowse_link when available -->
+              A. squamosa (Iridian Genomes)
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Amontana&tracks=DNA%2CAmontana_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              A. montana
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Aglabra&tracks=DNA%2CAglabra_gene_models" target="_blank" data-toggle="modal" data-target="#jbrowse_unavailable"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"> <!--Add jbrowse_link when available -->
+              A. glabra
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Mangifera indica</i></h5>
+          <div class="list-group">
+            <a href="https://mangobase.org/jbrowse/?data=data%2Fmango_ta4&tracks=DNA%2Cgene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              M. indica 'Tommy Atkins'
+              <span class="badge badge-warning badge-pill">MangoBase</span>
+            </a>
+            <a href="https://mangobase.org/jbrowse/?data=data%2Fmango_alphonso&tracks=DNA%2Cgene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              M. indica 'Alphonso'
+              <span class="badge badge-warning badge-pill">MangoBase</span>
+            </a>
+            <a href="https://ngdc.cncb.ac.cn/gwh/browser/index/Mangifera_indica/GWHABLA00000000" target="_blank" title="Genome Warehouse" 
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              M. indica 'Hong Xiang Ya'
+              <span class="badge badge-secondary badge-pill">GWH</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Persea americana</i></h5>
+          <div class="list-group">
+            <a href="https://www.avocado.uma.es/jbrowse/?data=data%2Fhass_uq&tracks=DNA%2Chass_uq_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              P. americana 'Hass' (UQ)
+              <span class="badge badge-warning badge-pill">AvoBase</span>
+            </a>
+            <a href="https://www.avocado.uma.es/jbrowse/?data=data%2Fgwen_ucr&tracks=DNA%2Cgwen_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              P. americana 'Gwen' (UCR)
+              <span class="badge badge-warning badge-pill">AvoBase</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Averrhoa carambola</i></h5>
+          <div class="list-group">
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/carambola&tracks=DNA%2Caveca_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              A. carambola
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Dimocarpus longan</i></h5>
+          <div class="list-group">
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data%2Flongan&tracks=DNA%2Cdimlo_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              D. longan 'Ji Dan Ben'
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+            <a href="https://www.sapindaceae.com/jbrowse/index.html?data=longanData&tracks=DNA%2Canno_gff3_final" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              D. longan 'Shi Xia'
+              <span class="badge badge-secondary badge-pill">SapBase</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Eriobotrya japonica</i></h5>
+          <div class="list-group">
+            <a href="https://ihsm-bionas.uma.es/jbrowse/index.html?data=data/loquat&tracks=DNA%2Cerija_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              E. japonica 'Guiye-23'
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+            <a href="https://www.rosaceae.org/jbrowse/index.html?data=data%2Feriobotrya%2FEjaponica_v1.0&tracks=DNA%2CEriobotrya-japonica_v1.0_gene%2CEriobotrya-japonica_v1.0_transcript" target="_blank" title="Genome Database for Rosaceae"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              E. japonica 'Big Five-pointed Star'
+              <span class="badge badge-secondary badge-pill">GDR</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Litchi chinensis</i></h5>
+          <div class="list-group">
+            <a href="https://www.sapindaceae.com/jbrowse/index.html?data=litchiData&tracks=DNA%2Canno_gff3" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              M. chinensis 'Feizixiao'
+              <span class="badge badge-secondary badge-pill">SapBase</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Carica papaya</i></h5>
+          <div class="list-group">
+            <a href="https://ihsm-bionas.uma.es/jbrowse/index.html?data=data/papaya&tracks=DNA%2Ccarpa_gene_models" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              C. papaya 'SunUp'
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+            <a href="https://www.rosaceae.org/jbrowse/index.html?data=data%2Feriobotrya%2FEjaponica_v1.0&tracks=DNA%2CEriobotrya-japonica_v1.0_gene%2CEriobotrya-japonica_v1.0_transcript" target="_blank" title="Genome Database for Rosaceae"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              E. japonica 'Big Five-pointed Star'
+              <span class="badge badge-secondary badge-pill">GDR</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <h5 style="color:#b52b27"><i>Theobroma cacao</i></h5>
+          <div class="list-group">
+            <a href="https://ihsm-bionas.uma.es/jbrowse/?data=data%2FCacao_Criollo&tracks=DNA%2CCriollo_Genes" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              T. cacao 'Criollo'
+              <span class="badge badge-success badge-pill">IHSMSubtropicals</span>
+            </a>
+            <a href="https://phytozome-next.jgi.doe.gov/jbrowse/index.html?data=genomes%2FTcacao_v2_1&tracks=Transcripts&highlight" target="_blank"
+               class="list-group-item list-group-item-action d-flex justify-content-between align-items-center jbrowse_link">
+              T. cacao 'Matina'
+              <span class="badge badge-secondary badge-pill">Phytozome</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="jbrowse_unavailable" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Currently unavailable</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        The <strong>Genome Browser</strong> will be included soon.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
