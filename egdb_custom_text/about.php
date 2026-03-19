d7b5268cf9f5be936476ccb5810a6e587284f95a
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2026-03-19 11:55:51 +0100

    about us updated

--- egdb_custom_text/about.php
+++ egdb_custom_text/about.php
@@ -4,9 +4,9 @@
   <h1 style="font-size:32px;" class="text-center">Welcome to IHSM<span style="color:#9c5;">Subtropicals</span> Fruits Database</h1>
 
   <p style="font-size:18px;margin-top:20px;" class="text-justify">
-    The Institute of Subtropical and Mediterranean Horticulture (<a href="https://www.ihsm.uma-csic.es/ihsm" target="_blank">IHSM</a>) "La Mayora" is a research center with more than 60 years of history and with a Research station of 50 Ha. Located in the province of Malaga, in southern Spain, it host a collection of varieties of subtropical species adapted to grow in the local environmental conditions (Mediterranean climate), outside of the tropics. The IHSM germplasm collection has the largest number of cherimoya acccessions of the world, and counts with more than 100 accessions of avocado and more than 80 of mango, among other species.
+    The Institute of Subtropical and Mediterranean Horticulture (<a href="https://www.ihsm.uma-csic.es/ihsm" target="_blank">IHSM</a>) "La Mayora" is a research center with more than 60 years of history and with a Research station of 25 Ha. Located in the province of Malaga, in southern Spain, it host a collection of varieties of subtropical species adapted to grow in the local environmental conditions (Mediterranean climate), outside the tropics. The IHSM germplasm collection has the largest number of cherimoya acccessions of the world (>300 accessions), and counts with more than 100 accessions of avocado and more than 80 of mango, among other species.
     <br>
-    This site provides bioinformatics tools and genomics data of subtropical species maintained and studied at the IHSM.
+    This site provides bioinformatics tools and genomics data of subtropical and other species of interest maintained and studied at the IHSM.
   </p>
   <br>
 
@@ -29,3 +29,36 @@
 <p>
   Our commitment lies in exploring the intricate dynamics between genetics, agriculture, and environmental sustainability. By integrating cutting-edge genomic approaches with traditional agricultural practices, we strive to pave the way towards a more resilient and fruitful future for subtropical fruit cultivation.
 </p>
+
+<h1>Funded by:</h1>
+
+<p>PID2021-125805OA-I00 funded by MICIU/AEI/10.13039/501100011033 and by “ERDF A way of making Europe”.</p>
+<img class="funding_logo" src="<?php echo $images_path.'/logos/miciu-aei.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path.'/logos/ERDF.png' ?>">
+<br>
+<br>
+<p>CNS2023-144643 funded by MCIN/AEI/10.13039/501100011033 and NextGenerationEU/PRTR.</p>
+<img class="funding_logo" src="<?php echo $images_path.'/logos/MCIN-AEI.jpg' ?>">
+<img class="funding_logo" src="<?php echo $images_path.'/logos/prtr.jpg' ?>">
+<img class="funding_logo" src="<?php echo $images_path.'/logos/nextgeneu.jpg' ?>">
+<br>
+<br>
+<p>20224AT004 funded by MCIN/AEI/10.13039/501100011033, CSIC and IHSM.</p>
+<img class="funding_logo" src="<?php echo $images_path.'/logos/MCIN-AEI.jpg' ?>">
+<img class="funding_logo" src="<?php echo $images_path.'/logos/csic_logo.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path.'/logos/ihsm_logo.png' ?>">
+<br>
+<br>
+<p>INVESTIGO program, within the Recovery, Transformation and Resilience Plan, funded by the European Union - NextGenerationEU.</p>
+<img class="funding_logo" src="<?php echo $images_path.'/logos/junta_andalucia.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path.'/logos/prtr.jpg' ?>">
+<img class="funding_logo" src="<?php echo $images_path.'/logos/nextgeneu.jpg' ?>">
+<br>
+<br>
+
+<style>
+  .funding_logo {
+    height:80px;
+    margin-right:20px;
+  }
+</style>
