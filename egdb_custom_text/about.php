d0144f5f2057eda229284733deaea680442a55f1
Enrique López Gómez <enrique.elg.93@gmail.com>
2026-04-19 01:45:43 +0200

    Update about.php

--- egdb_custom_text/about.php
+++ egdb_custom_text/about.php
@@ -1,64 +1,83 @@
+<br>
 
-  <br>
+<h1 style="font-size:32px;" class="text-center">Welcome to IHSM<span style="color:#9c5;">Subtropicals</span> Fruits Database</h1>
 
-  <h1 style="font-size:32px;" class="text-center">Welcome to IHSM<span style="color:#9c5;">Subtropicals</span> Fruits Database</h1>
-
-  <p style="font-size:18px;margin-top:20px;" class="text-justify">
+<p style="font-size:18px;margin-top:20px;" class="text-justify">
     The Institute of Subtropical and Mediterranean Horticulture (<a href="https://www.ihsm.uma-csic.es/ihsm" target="_blank">IHSM</a>) "La Mayora" is a research center with more than 60 years of history and with a Research station of 25 Ha. Located in the province of Malaga, in southern Spain, it host a collection of varieties of subtropical species adapted to grow in the local environmental conditions (Mediterranean climate), outside the tropics. The IHSM germplasm collection has the largest number of cherimoya acccessions of the world (>300 accessions), and counts with more than 100 accessions of avocado and more than 80 of mango, among other species.
     <br>
     This site provides bioinformatics tools and genomics data of subtropical and other species of interest maintained and studied at the IHSM.
-  </p>
-  <br>
+</p>
+<br>
 
-  <?php 
-    include_once realpath("$custom_text_path/db_citation.php");
-  ?>
-
-  <br>
+<?php
+include_once realpath("$custom_text_path/db_citation.php");
+?>
+<br>
 
 
 <p>
-  Our group's activity revolves around the sustainable utilization of genetic resources in subtropical fruit trees, considering both current conditions and future climate change projections. This initiative stands as a unique endeavor within the CSIC. We collect detailed knowledge of plant materials, advanced cultivation techniques, and the utilization of genomic tools to pursue the following specific objectives:
+    Our group's activity revolves around the sustainable utilization of genetic resources in subtropical fruit trees,
+    considering both current conditions and future climate change projections. This initiative stands as a unique
+    endeavor within the CSIC. We collect detailed knowledge of plant materials, advanced cultivation techniques, and the
+    utilization of genomic tools to pursue the following specific objectives:
 </p>
 <ol>
-  <li>Study, conservation, and utilization of plant genetic resources.</li>
-  <li>Exploration of reproductive biology implications in fruit setting.</li>
-  <li>Development of innovative cultural technologies to enhance productivity and quality.</li>
-  <li>Selection of plant materials for incorporation into improvement programs via classical and biotechnological methods.</li>
+    <li>Study, conservation, and utilization of plant genetic resources.</li>
+    <li>Exploration of reproductive biology implications in fruit setting.</li>
+    <li>Development of innovative cultural technologies to enhance productivity and quality.</li>
+    <li>Selection of plant materials for incorporation into improvement programs via classical and biotechnological
+        methods.</li>
 </ol>
 <p>
-  Our commitment lies in exploring the intricate dynamics between genetics, agriculture, and environmental sustainability. By integrating cutting-edge genomic approaches with traditional agricultural practices, we strive to pave the way towards a more resilient and fruitful future for subtropical fruit cultivation.
+    Our commitment lies in exploring the intricate dynamics between genetics, agriculture, and environmental
+    sustainability. By integrating cutting-edge genomic approaches with traditional agricultural practices, we strive to
+    pave the way towards a more resilient and fruitful future for subtropical fruit cultivation.
 </p>
+
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+        <?php
+        if ($ab_labs && file_exists(realpath("groups.php"))) {
+            include_once realpath("groups.php");
+        }
+        ?>
+    </div>
+</div>
+
+
 
 <h1>Funded by:</h1>
 
 <p>PID2021-125805OA-I00 funded by MICIU/AEI/10.13039/501100011033 and by “ERDF A way of making Europe”.</p>
-<img class="funding_logo" src="<?php echo $images_path.'/logos/miciu-aei.png' ?>">
-<img class="funding_logo" src="<?php echo $images_path.'/logos/ERDF.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/miciu-aei.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/ERDF.png' ?>">
 <br>
 <br>
 <p>CNS2023-144643 funded by MICIU/AEI/10.13039/501100011033 and NextGenerationEU/PRTR.</p>
-<img class="funding_logo" src="<?php echo $images_path.'/logos/miciu-aei.png' ?>">
-<img class="funding_logo" src="<?php echo $images_path.'/logos/prtr.jpg' ?>">
-<img class="funding_logo" src="<?php echo $images_path.'/logos/nextgeneu.jpg' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/miciu-aei.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/prtr.jpg' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/nextgeneu.jpg' ?>">
 <br>
 <br>
 <p>20224AT004 funded by MICIU/AEI/10.13039/501100011033, CSIC and IHSM.</p>
-<img class="funding_logo" src="<?php echo $images_path.'/logos/miciu-aei.png' ?>">
-<img class="funding_logo" src="<?php echo $images_path.'/logos/csic_logo.png' ?>">
-<img class="funding_logo" src="<?php echo $images_path.'/logos/ihsm_logo.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/miciu-aei.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/csic_logo.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/ihsm_logo.png' ?>">
 <br>
 <br>
-<p>INVESTIGO program, within the Recovery, Transformation and Resilience Plan, funded by the European Union - NextGenerationEU.</p>
-<img class="funding_logo" src="<?php echo $images_path.'/logos/junta_andalucia.png' ?>">
-<img class="funding_logo" src="<?php echo $images_path.'/logos/prtr.jpg' ?>">
-<img class="funding_logo" src="<?php echo $images_path.'/logos/nextgeneu.jpg' ?>">
+<p>INVESTIGO program, within the Recovery, Transformation and Resilience Plan, funded by the European Union -
+    NextGenerationEU.</p>
+<img class="funding_logo" src="<?php echo $images_path . '/logos/junta_andalucia.png' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/prtr.jpg' ?>">
+<img class="funding_logo" src="<?php echo $images_path . '/logos/nextgeneu.jpg' ?>">
 <br>
 <br>
 
 <style>
-  .funding_logo {
-    height:80px;
-    margin-right:20px;
-  }
+    .funding_logo {
+        height: 80px;
+        margin-right: 20px;
+    }
 </style>
