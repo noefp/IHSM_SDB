1b49e626b14e2eb0bc9e03676e73e5e133d9a9a9
Noé Fernández Pozo <noeisneo@gmail.com>
2026-05-04 12:51:07 +0200

    changed species subtitle text

---
+++ egdb_custom_text/custom_titles/custom_species_title.php
@@ -0,0 +1,5 @@
+<br>
+<h1 style="font-size:32px;text-align:center">Species</h1>
+<p style="margin:auto; font-size:22px; max-width:1200px; text-align:center; margin-bottom:20px">
+<strong><span>IHSM</span><span style="color:#9c5">Subtropicals</span></strong> hosts genomic resources and bioinformatics tools for subtropical species of research interest at the <a href="https://www.ihsm.uma-csic.es/" target="_blank">Instituto de Hortofruticultura Subtropical y Mediterránea "La Mayora"</a>.
+</p>
