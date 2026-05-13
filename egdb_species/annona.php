cc74a38772e9c571e25f5774219b8144f3abdc8c
Enrique López Gómez <enrique.elg.93@gmail.com>
2026-05-13 17:42:54 +0200

    Annona spp page updated

--- egdb_species/annona.php
+++ egdb_species/annona.php
@@ -2,7 +2,21 @@
 <img class="float-right" style="z-index:0;height:150px" src="<?php echo $images_path . '/species/' . $sps_img ?>">
 <h1><?php echo $sps_subtitle ?></h1>
 <h3 style="color:#666"><em><?php echo $sps_title ?></em></h3>
-<a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/anche102" target="_blank" class="btn btn-info sps-btn" role="button">Genome Browser</a>
+
+<div class="dropdown">
+   <a href="#" class="btn btn-info sps-btn dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Genome Browser
+   </a>
+   <div class="dropdown-menu bg-info">
+      <a class="dropdown-item text-white btn-hover" href="https://ihsm-bionas.uma.es/jbrowse/?data=data/anche102&tracks=DNA%2Canche102_genes" target="_blank">A. cherimola 'Fino de Jete'</a>
+      <a class="dropdown-item text-white btn-hover" href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Acherimola_booth&tracks=DNA%2CAcherimola_booth_gene_models" target="_blank">A. cherimola 'Booth'</a>
+      <a class="dropdown-item text-white btn-hover" href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Annona_glabra&tracks=DNA%2CAglabra_gene_models" target="_blank">A. glabra</a>
+      <a class="dropdown-item text-white btn-hover" href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Amontana&tracks=DNA%2CAmontana_gene_models" target="_blank">A. montana</a>
+      <a class="dropdown-item text-white btn-hover" href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Amuricata&tracks=DNA%2Camuricata_gene_models" target="_blank">A. muricata</a>
+      <a class="dropdown-item text-white btn-hover" href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Annona_squamosa&tracks=DNA%2CAsquamosa_gene_models" target="_blank">A. squamosa</a>
+  </div>
+</div>
+
 <a href="/easy_gdb/tools/blast/blast_input.php" class="btn btn-info sps-btn" role="button">BLAST</a>
 <a href="/easy_gdb/tools/fasta_download.php" class="btn btn-info sps-btn" role="button">Sequence Extraction</a>
 <a href="/easy_gdb/tools/annot_input_list.php" class="btn btn-info sps-btn" role="button">Annotation Extraction</a>
@@ -10,107 +24,61 @@
 <br style="clear:both" />
 
 <p>
-    <em>Annona cherimola</em> (cherimoya) is a diploid fruit species (2n=2x=14) native to Mesoamerica. It is denominated as cherimoya or "chirimoya" in South America and "anona" in Central America, as other closely related species. This species has been cultivated and appreciated since pre-Columbian times by the Maya, Olmec, and Izapa civilizations.
+   The genus <em>Annona</em> (Annonaceae) comprises over 120 species, many of which are of significant ethnobotanical and agricultural importance. While major crops such as <em>A. cherimola</em>, <em>A. muricata</em>, and <em>A. squamosa</em> are extensively studied, other wild and semi-domesticated species like <strong><em>A. glabra</em></strong> and <strong><em>A. montana</em></strong> represent vital genetic reservoirs for crop improvement and pharmacological research.
 </p>
-
 <ul>
-    <li>
-        <strong>Floral Biology:</strong>
-        Cherimoya flowers are hermaphroditic with protogynous dichogamy, where female and male structures mature at different times to prevent self-fertilization and enhance cross-pollination.
-    </li>
-
-    <li>
-        <strong>Evolutionary Significance:</strong>
-        The genus <em>Annona</em> belongs to the order Magnoliales, one of the earliest lineages of flowering plants. Its primitive floral characteristics and high genetic variability provide insights into the evolution of angiosperms and their adaptation to subtropical ecosystems.
-    </li>
-
-    <li>
-        <strong>Geographic Production:</strong>
-        Spain is the main commercial producer with 3,000 ha, followed by Peru and Chile. It is also cultivated on a smaller scale in California (USA), Argentina, Madeira (Portugal), South Africa, Australia, Italy, Morocco, Algeria, Egypt, and Israel.
-    </li>
-
-    <li>
-        <strong>Germplasm Conservation:</strong>
-        The <a href="https://www.ihsm.uma-csic.es/" target="_blank">Institute of Subtropical and Mediterranean Horticulture "La Mayora" (IHSM)</a> holds the largest germplasm collection of <em>A. cherimola</em>, with more than 300 accessions.
-    </li>
-
-    <li>
-        <strong>Medicinal Importance:</strong>
-        Cherimoya contains polyketide secondary metabolites, such as acetogenins, found in seeds and leaves. These compounds exhibit antitumor, antimalarial, and pesticidal properties.
-    </li>
+   <li>
+      <strong><em>Annona glabra</em> (Pond Apple):</strong>
+      Commonly found in swampy and coastal areas of the Americas and West Africa, this species is noted for its high tolerance to waterlogging and salinity. It serves as a crucial rootstock for other <em>Annona</em> species to improve flood resistance in commercial orchards.
+   </li>
+   <li>
+      <strong><em>Annona montana</em> (Mountain Soursop):</strong>
+      Native to the Amazon and the West Indies, <em>A. montana</em> is a hardy species capable of growing at higher altitudes and colder temperatures than <em>A. muricata</em>. It is frequently utilized in breeding programs to enhance vigor and cold hardiness in subtropical cultivars.
+   </li>
+   <li>
+      <strong>Evolutionary Significance:</strong>
+      The wild species of <em>Annona</em> belong to the early-divergent Magnoliid clade. Studying their genomes provides essential insights into the primitive floral traits and the diversification of angiosperms within subtropical and tropical ecosystems.
+   </li>
+   <li>
+      <strong>Ecological Importance:</strong>
+      These species play a fundamental role in their native habitats as food sources for local fauna. Furthermore, their diverse secondary metabolite profiles, including acetogenins and alkaloids, are increasingly investigated for their antimicrobial and cytotoxic activities.
+   </li>
+   <li>
+      <strong>Genomic Resources:</strong>
+      Chromosome-level assemblies for both <em>A. glabra</em> and <em>A. montana</em> have been developed, facilitating comparative genomic studies within the genus and the identification of stress-tolerance genes that are absent in more domesticated species.
+   </li>
 </ul>
 
 <br>
 
-<h3>Documents</h3>
-<div class="row" style="width:900px;margin-left:0px">
-    <div class="col" style="padding:0px">
-        <div class="card">
-            <div class="card-body">
-                <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/descriptores para chirimoyo.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Descriptores para chirimoyo</a>
-            </div>
-        </div>
-    </div>
-    <div class="col" style="padding:0px">
-        <div class="card">
-            <div class="card-body">
-                <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/descriptors for cherimoya.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Descriptors for cherimoya</a>
-            </div>
-        </div>
-    </div>
-    <div class="col" style="padding:0px">
-        <div class="card">
-            <div class="card-body">
-                <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/guia chirimoyo plagas.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Guia de gestion de plagas</a>
-            </div>
-        </div>
-    </div>
-</div>
+<h3>Genome assemblies</h3>
+<p>
+   There are several sequenced genomes of <em>Annona</em> spp. publicly available:
+</p>
+<ul>
+   <li> <em>Annona cherimola</em> 'Fino de Jete', by <a href="https://doi.org/10.1002/ppp3.10366" target="_blank">Talavera et al., 2023</a>. Available at <a href="https://www.ncbi.nlm.nih.gov/datasets/genome/GCA_035061585.1/" target="_blank">NCBI</a>.</li>
+   <li> <em>Annona cherimola</em> 'Booth', by <a href="https://doi.org/10.3390/plants13050636" target="_blank">Li et al., 2024</a>. Available at <a href="https://www.ncbi.nlm.nih.gov/datasets/genome/GCA_030490835.1/" target="_blank">NCBI</a>.</li>
+   <li> <em>Annona glabra</em>, by <a href="https://doi.org/10.1038/s41559-022-01744-9" target="_blank">He et al., 2022</a>. Available at <a href="https://ngdc.cncb.ac.cn/gwh/Assembly/20680/show/" target="_blank">Genome Warehouse</a>.</li>
+   <li> <em>Annona montana</em>, by <a href="https://doi.org/10.48130/OPR-2023-0014" target="_blank">Tang et al., 2023</a>. Available at <a href="https://ngdc.cncb.ac.cn/gwh/Assembly/65977/show" target="_blank">Genome Warehouse</a>.</li>
+   <li> <em>Annona muricata</em>, by <a href="https://doi.org/10.1111/1755-0998.13353" target="_blank">Strijk et al., 2021</a>. Raw reads available at <a href="https://www.ebi.ac.uk/ena/browser/view/PRJEB30626" target="_blank">ENA</a>.</li>
+   <li> <em>Annona squamosa</em>, by <a href="https://doi.org/10.56179/001c.116283" target="_blank">Peña-Ramirez et al., 2024</a>. Available at <a href="https://www.ncbi.nlm.nih.gov/datasets/genome/GCA_035584095.1/" target="_blank">NCBI</a>.</li>
+   <li> <em>Annona squamosa</em>, by <a href="https://doi.org/10.1007/s00497-023-00464-9" target="_blank">Alves-Rodrigues et al., 2023</a>.</li>
+   <li> <em>Annona squamosa</em>, by <a href="https://doi.org/10.1093/dnares/dsaf007" target="_blank"> Bisht et al., 2025</a>.</li>
+</ul>
 
-<div class="row" style="width:900px;margin-left:0px">
-    <div class="col" style="padding:0px">
-        <div class="card">
-            <div class="card-body">
-                <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/chirimoya_fao_ficha_tecnica_2006.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Ficha técnica FAO 2006</a>
-            </div>
-        </div>
-    </div>
-    <div class="col" style="padding:0px">
-        <div class="card">
-            <div class="card-body">
-                <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/chirimoya_valor_nutricional_fen_org.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Valor nutricional FEN.org.es</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="row" style="width:900px;margin-left:0px">
-    <div class="col" style="padding:0px">
-        <div class="card">
-            <div class="card-body">
-                <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/Cherla_Spanish.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Optimizacion de tecnicas de cultivo</a>
-            </div>
-        </div>
-    </div>
-    <div class="col" style="padding:0px">
-        <div class="card">
-            <div class="card-body">
-                <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/Cherla_English.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Optimization of cultural techniques</a>
-            </div>
-        </div>
-    </div>
-</div>
-<br>
 <br>
 
 <h3>Genome assembly stats</h3>
 <p>
-    Summary statistics of the <em>Annona cherimola</em> genome assemblies for cv. 'Fino de Jete' (<a href="https://doi.org/10.1002/ppp3.10366" target="_blank">Talavera et al., 2023</a>) and cv. 'Booth' (<a href="https://doi.org/10.3390/plants13050636" target="_blank">Li et al. 2024</a>).
+   Overall statistics on <em>Annona</em> genome assemblies published by <a href="https://doi.org/10.1002/ppp3.10366" target="_blank">Talavera et al., 2023</a>, by <a href="https://doi.org/10.3390/plants13050636" target="_blank">Li et al., 2024</a>, by <a href="https://doi.org/10.1038/s41559-022-01744-9" target="_blank">He et al., 2022</a>, by <a href="https://doi.org/10.48130/OPR-2023-0014" target="_blank">Tang et al., 2023</a>, by <a href="https://doi.org/10.1111/1755-0998.13353" target="_blank">Strijk et al., 2021</a>, and by <a href="https://doi.org/10.56179/001c.116283" target="_blank">Peña-Ramirez et al., 2024</a>.
 </p>
-<?php include_once("cherimoya_genome_table.php"); ?>
+
+<?php include_once("annona_genome_table.php"); ?>
 * Reference genome for IHSM Subtropicals
 <br>
 <sup>a</sup> BUSCO based on embryophyta_odb12 dataset
+<br>
+<sup>b</sup> BUSCO based on <a href="https://github.com/usadellab/Helixer" target="_blank">Helixer</a> prediction
 <br><br>
 
 <h3>Taxonomy</h3>
@@ -119,7 +87,6 @@
 
 <h3>Links of interest</h3>
 <ul>
-    <li><a href="https://bancocrf.inia.es/es/accessions?institute_code=ESP058&taxon=Annona&rank=genus" target="_blank">Cherimoya Spanish Germplasm Bank</a></li>
-    <li><a href="https://www.cabi.org/isc/datasheet/5806" target="_blank">Cherimoya Datasheet at CABI</a></li>
+   <li><a href="https://www.cabi.org/isc/datasheet/5801" target="_blank">Annona Datasheet at CABI</a></li>
 </ul>
 <br>
--- egdb_species/annona_genome_table.php
+++ egdb_species/annona_genome_table.php
@@ -1,58 +1,87 @@
-
-      <table class="table table-bordered">
-         <thead>
-           <tr>
-             <th></th>
-             <th>Fino de Jete*</th>
-             <th>Booth</th>
-           </tr>
-         </thead>
-         <tbody>
-          <tr>
-             <td>Sequence count</td>
-             <td>2,052</td>
-             <td>1,377</td>
-           </tr>
-           <tr>
-             <td>Total length (Mb)</td>
-             <td>1,137.39</td>
-             <td>794.02</td>
-           </tr>
-           <tr>
-             <td>Longest sequence (Mb)</td>
-             <td>212.25</td>
-             <td>128.58</td>
-           </tr>
-           <tr>
-             <td>Average length (Mb)</td>
-             <td>0.55</td>
-             <td>0.58</td>
-           </tr>
-           <tr>
-             <td>Number of Ns (%)</td>
-             <td>0.58</td>
-             <td>0.00</td>
-           </tr>
-           <tr>
-             <td>GC (%)</td>
-             <td>34.69</td>
-             <td>35.25</td>
-           </tr>
-           <tr>
-             <td>N50 (Mb)</td>
-             <td>170.86</td>
-             <td>97.59</td>
-           </tr>
-           <tr>
-             <td>L50 (sequences)</td>
-             <td>3</td>
-             <td>4</td>
-           </tr>
-           <tr>
-             <td>BUSCO completeness<sup>a</sup> (%)</td>
-             <td>90.62</td>
-             <td>92.30</td>
-           </tr>
-         </tbody>
-       </table>
-      
+<table class="table table-bordered">
+   <thead>
+      <tr>
+         <th>Metric</th>
+         <th><em>A. cherimola</em> 'Fino de Jete'*</th>
+         <th><em>A. cherimola</em> 'Booth'</th>
+         <th><em>A. glabra</em></th>
+         <th><em>A. montana</em></th>
+         <th><em>A. muricata</em></th>
+         <th><em>A. squamosa</em></th>
+      </tr>
+   </thead>
+   <tbody>
+      <tr>
+         <td>Sequence count</td>
+         <td>2,052</td>
+         <td>1,377</td>
+         <td>66</td>
+         <td>348</td>
+         <td>673</td>
+         <td>64,832</td>
+      </tr>
+      <tr>
+         <td>Total length (Mb)</td>
+         <td>1,137.39</td>
+         <td>794.02</td>
+         <td>1,027.31</td>
+         <td>973.58</td>
+         <td>654.23</td>
+         <td>587.83</td>
+      </tr>
+      <tr>
+         <td>Longest sequence (Mb)</td>
+         <td>212.25</td>
+         <td>128.58</td>
+         <td>103.20</td>
+         <td>185.37</td>
+         <td>122.60</td>
+         <td>94.51</td>
+      </tr>
+      <tr>
+         <td>Average length (Mb)</td>
+         <td>0.55</td>
+         <td>0.58</td>
+         <td>15.57</td>
+         <td>2.80</td>
+         <td>0.97</td>
+         <td>9.07E-03</td>
+      </tr>
+      <tr>
+         <td>GC (%)</td>
+         <td>34.69</td>
+         <td>35.25</td>
+         <td>33.83</td>
+         <td>35.49</td>
+         <td>34.56</td>
+         <td>33.94</td>
+      </tr>
+      <tr>
+         <td>N50 (Mb)</td>
+         <td>170.86</td>
+         <td>97.59</td>
+         <td>75.19</td>
+         <td>144.84</td>
+         <td>93.26</td>
+         <td>58.02</td>
+      </tr>
+      <tr>
+         <td>L50 (sequences)</td>
+         <td>3</td>
+         <td>4</td>
+         <td>6</td>
+         <td>3</td>
+         <td>3</td>
+         <td>4</td>
+      </tr>
+      <tr>
+         <td>BUSCO completeness<sup>a</sup> (%)</td>
+         <td>90.62</td>
+         <td>92.30</td>
+         <td>94.57<sup>b</sup></td>
+         <td>89.83</td>
+         <td>89.73<sup>b</sup></td>
+         <td>87.61<sup>b</sup></td>
+      </tr>
+   </tbody>
+</table>
