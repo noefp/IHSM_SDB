33a08dc00d6fc2fe011a68eedf61651b27e5fbaf
Enrique López Gómez <enrique.elg.93@gmail.com>
2026-05-13 12:16:51 +0200

    Squamosa page updated

--- egdb_species/squamosa.php
+++ egdb_species/squamosa.php
@@ -2,7 +2,7 @@
 <img class="float-right" style="z-index:0;height:150px" src="<?php echo $images_path.'/species/'.$sps_img ?>" >
 <h1><?php echo $sps_subtitle ?></h1>
 <h3 style="color:#666"><em><?php echo $sps_title ?></em></h3>
-<a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/anche102" target="_blank" class="btn btn-info sps-btn" role="button">Genome Browser</a>
+<a href="https://ihsm-bionas.uma.es/jbrowse/?data=data/Annona_squamosa&tracks=DNA%2CAsquamosa_gene_models" target="_blank" class="btn btn-info sps-btn" role="button">Genome Browser</a>
 <a href="/easy_gdb/tools/blast/blast_input.php" class="btn btn-info sps-btn" role="button">BLAST</a>
 <a href="/easy_gdb/tools/fasta_download.php" class="btn btn-info sps-btn" role="button">Sequence Extraction</a>
 <a href="/easy_gdb/tools/annot_input_list.php" class="btn btn-info sps-btn" role="button">Annotation Extraction</a>
@@ -10,107 +10,37 @@
 <br style="clear:both"/>
 
 <p>
-  <em>Annona cherimola</em> (cherimoya) is a diploid fruit species (2n=2x=14) native to Mesoamerica. It is denominated as cherimoya or "chirimoya" in South America and "anona" in Central America, as other closely related species. This species has been cultivated and appreciated since pre-Columbian times by the Maya, Olmec, and Izapa civilizations.
+   <em>Annona squamosa</em> (sugar apple) is a diploid fruit species (2n=2x=14) native to the tropical Americas and the West Indies. Commonly known as "anón", "saramuyo", "sweetsop", or "sugar apple", it is the most widely cultivated species of the <em>Annona</em> genus worldwide, prized for its sweet, custard-like pulp and its adaptability to diverse tropical climates.
 </p>
-
 <ul>
-  <li>
-    <strong>Floral Biology:</strong>  
-    Cherimoya flowers are hermaphroditic with protogynous dichogamy, where female and male structures mature at different times to prevent self-fertilization and enhance cross-pollination.
-  </li>
-
-  <li>
-    <strong>Evolutionary Significance:</strong>
-    The genus <em>Annona</em> belongs to the order Magnoliales, one of the earliest lineages of flowering plants. Its primitive floral characteristics and high genetic variability provide insights into the evolution of angiosperms and their adaptation to subtropical ecosystems.
-  </li>
-
-  <li>
-    <strong>Geographic Production:</strong>
-    Spain is the main commercial producer with 3,000 ha, followed by Peru and Chile. It is also cultivated on a smaller scale in California (USA), Argentina, Madeira (Portugal), South Africa, Australia, Italy, Morocco, Algeria, Egypt, and Israel.
-  </li>
-
-  <li>
-    <strong>Germplasm Conservation:</strong>  
-    The <a href="https://www.ihsm.uma-csic.es/" target="_blank">Institute of Subtropical and Mediterranean Horticulture "La Mayora" (IHSM)</a> holds the largest germplasm collection of <em>A. cherimola</em>, with more than 300 accessions.
-  </li>
-
-  <li>
-    <strong>Medicinal Importance:</strong>  
-    Cherimoya contains polyketide secondary metabolites, such as acetogenins, found in seeds and leaves. These compounds exhibit antitumor, antimalarial, and pesticidal properties.
-  </li>
+<li>
+<strong>Floral Biology:</strong>
+   The flowers are hermaphroditic and exhibit protogynous dichogamy. Similar to other Annonaceae, the female stage precedes the male stage, a mechanism that promotes cross-pollination. In many regions, natural pollination is primarily carried out by nitidulid beetles.
+</li>
+<li>
+<strong>Evolutionary Significance:</strong>
+   As a representative of the Magnoliid clade, <em>A. squamosa</em> occupies a pivotal position in angiosperm evolution. Its relatively small genome serves as a fundamental model for studying the divergence of early flowering plants and the genetic basis of fruit development in the <em>Annona</em> lineage.
+</li>
+<li>
+<strong>Geographic Production:</strong>
+   It is widely distributed across the tropics. India and China are currently the largest commercial producers, with significant cultivation also found in the tropical regions of Africa and the Pacific.
+</li>
+<li>
+<strong>Medicinal Importance:</strong>
+   Sugar apple is a rich source of secondary metabolites like alkaloids, flavonoids, and tannins. Extracts from roots, bark, leaves, and fruit possess extensive ethnomedicinal value. These tissues exhibit potent anticancer, antioxidant, antiparasitic, and antimalarial activities, alongside antidiabetic, antihypertensive, hepatoprotective, insecticidal, microbicidal, and molluscicidal properties.
+</li>
 </ul>
 
 <br>
 
-<h3>Documents</h3>
-<div class="row" style="width:900px;margin-left:0px">
-  <div class="col" style="padding:0px">
-    <div class="card">
-      <div class="card-body">
-         <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/descriptores para chirimoyo.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Descriptores para chirimoyo</a>
-      </div>
-    </div>
-  </div>
-  <div class="col" style="padding:0px">
-    <div class="card">
-      <div class="card-body">
-        <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/descriptors for cherimoya.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Descriptors for cherimoya</a>
-      </div>
-    </div>
-  </div>
-  <div class="col" style="padding:0px">
-    <div class="card">
-      <div class="card-body">
-        <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/guia chirimoyo plagas.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Guia de gestion de plagas</a>
-      </div>
-    </div>
-  </div>
-</div>
-  
-<div class="row" style="width:900px;margin-left:0px">
-  <div class="col" style="padding:0px">
-    <div class="card">
-      <div class="card-body">
-        <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/chirimoya_fao_ficha_tecnica_2006.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Ficha técnica FAO 2006</a>
-      </div>
-    </div>
-  </div>
-  <div class="col" style="padding:0px">
-    <div class="card">
-      <div class="card-body">
-        <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/chirimoya_valor_nutricional_fen_org.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Valor nutricional FEN.org.es</a>
-      </div>
-    </div>
-  </div>
-</div>
-  
-<div class="row" style="width:900px;margin-left:0px">
-  <div class="col" style="padding:0px">
-    <div class="card">
-      <div class="card-body">
-        <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/Cherla_Spanish.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Optimizacion de tecnicas de cultivo</a>
-      </div>
-    </div>
-  </div>
-  <div class="col" style="padding:0px">
-    <div class="card">
-      <div class="card-body">
-        <a href="https://ihsm-bionas.uma.es/downloads/Annona%20cherimola/PDFs/Cherla_English.pdf"><em class='far fa-file-pdf' style='font-size:30px;color:red'></em> Optimization of cultural techniques</a>
-      </div>
-    </div>
-  </div>
-</div>
-<br>
-<br>
-
 <h3>Genome assembly stats</h3>
 <p>
-  Summary statistics of the <em>Annona cherimola</em> genome assemblies for cv. 'Fino de Jete' (<a href="https://doi.org/10.1002/ppp3.10366" target="_blank">Talavera et al., 2023</a>) and cv. 'Booth' (<a href="https://doi.org/10.3390/plants13050636" target="_blank">Li et al. 2024</a>).
+   Summary statistics of the <em>Annona squamosa</em> reference genome assembly (<a href="https://doi.org/10.56179/001c.116283" target="_blank">Peña-Ramírez et al., 2024</a>).
 </p>
-<?php include_once("cherimoya_genome_table.php");?>
-* Reference genome for IHSM Subtropicals
-<br>
+<?php include_once("squamosa_genome_table.php");?>
 <sup>a</sup> BUSCO based on embryophyta_odb12 dataset
+<br>
+<sup>b</sup> BUSCO based on <a href="https://github.com/usadellab/Helixer" target="_blank">Helixer</a> prediction
 <br><br>
 
 <h3>Taxonomy</h3>
@@ -119,7 +49,6 @@
 
 <h3>Links of interest</h3>
 <ul>
-  <li><a href="https://bancocrf.inia.es/es/accessions?institute_code=ESP058&taxon=Annona&rank=genus" target="_blank">Cherimoya Spanish Germplasm Bank</a></li>
-  <li><a href="https://www.cabi.org/isc/datasheet/5806" target="_blank">Cherimoya Datasheet at CABI</a></li>
+    <li><a href="https://www.cabi.org/isc/datasheet/5820" target="_blank">Sugar apple Datasheet at CABI</a></li>
 </ul>
 <br>
--- egdb_species/squamosa_genome_table.php
+++ egdb_species/squamosa_genome_table.php
@@ -2,56 +2,46 @@
       <table class="table table-bordered">
          <thead>
            <tr>
-             <th></th>
-             <th>Fino de Jete*</th>
-             <th>Booth</th>
+             <th>Metric</th>
+             <th>Annona squamosa</th>
            </tr>
          </thead>
          <tbody>
           <tr>
              <td>Sequence count</td>
-             <td>2,052</td>
-             <td>1,377</td>
+             <td>64,832</td>
            </tr>
            <tr>
              <td>Total length (Mb)</td>
-             <td>1,137.39</td>
-             <td>794.02</td>
+             <td>587.83</td>
            </tr>
            <tr>
              <td>Longest sequence (Mb)</td>
-             <td>212.25</td>
-             <td>128.58</td>
+             <td>94.51</td>
            </tr>
            <tr>
              <td>Average length (Mb)</td>
-             <td>0.55</td>
-             <td>0.58</td>
+             <td>9.07E-03</td>
            </tr>
            <tr>
              <td>Number of Ns (%)</td>
-             <td>0.58</td>
-             <td>0.00</td>
+             <td>3.67</td>
            </tr>
            <tr>
              <td>GC (%)</td>
-             <td>34.69</td>
-             <td>35.25</td>
+             <td>33.94</td>
            </tr>
            <tr>
              <td>N50 (Mb)</td>
-             <td>170.86</td>
-             <td>97.59</td>
+             <td>58.02</td>
            </tr>
            <tr>
              <td>L50 (sequences)</td>
-             <td>3</td>
              <td>4</td>
            </tr>
            <tr>
-             <td>BUSCO completeness<sup>a</sup> (%)</td>
-             <td>90.62</td>
-             <td>92.30</td>
+             <td>BUSCO completeness<sup>a,b</sup> (%)</td>
+             <td>87.61</td>
            </tr>
          </tbody>
        </table>
