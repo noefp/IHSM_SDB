c58037a538f33c0723e0ea09f99dc3af38a3f03b
Noé Fernández Pozo <noeisneo@gmail.com>
2026-07-13 09:09:05 +0200

    CV calculator enabled

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -20,6 +20,7 @@
   <div class="dropdown-menu">
     <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_input.php">Experiment Expression Viewer</a>
     <a class="dropdown-item" href="/easy_gdb/tools/expression/comparator_input.php">Expression comparator</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/expression/cv_calculator_input.php">CV calculator</a>
     <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_menu.php">Datasets</a>
   </div>
 </li>
